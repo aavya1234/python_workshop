5f22f12b01181119a150523e98c8bcdd90e30515
aavya1234 <149650708+aavya1234@users.noreply.github.com>
2023-11-02 13:15:55 +0530

    Add files via upload

---
+++ player.py
@@ -0,0 +1,6 @@
+player_status={"name":"player 1", "score":100, "level":2}
+def display_player_info():
+    print(f"name:{player_status['name']}")
+    print(f"score:{player_status['score']}")
+    print(f"level:{player_status['level']}")
+display_player_info()
